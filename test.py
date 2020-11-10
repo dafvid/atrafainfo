4788f3ad378ddc65a1ff6e073e066942cdd80d41
David S <david@dafnet.se>
2020-11-10 23:40:58 +0100

    fixes #5 update test data path

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+import os
+
 from atrafadata import export
 
-server_path = 'test'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+server_path = os.path.join(dir_path, 'test')
 
 
 with open('web/index.html', 'w') as f:
