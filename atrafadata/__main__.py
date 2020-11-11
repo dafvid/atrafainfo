47db006688b924bb0d3c4e22e471134074f7c807
David S <david@dafnet.se>
2020-11-11 11:57:27 +0100

    Check if data path exists fixes #7

--- atrafadata/__main__.py
+++ atrafadata/__main__.py
@@ -1,8 +1,15 @@
+import os
+import sys
+
 from atrafadata import export
 
 server_path = '/var/opt/minecraft/server'
 x = (258, 311)
 z = (-50, 50)
+
+
+if not os.path.exists(server_path):
+    sys.exit("Can't find minecraft data folder ({})".format(server_path))
 
 print(export(server_path))
 
