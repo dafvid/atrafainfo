8d92bfcf01589fae295a7651a525e7e585dc1340
David S <david@dafnet.se>
2020-11-11 23:07:49 +0100

    setup_web now works from anywhere

--- setup_web.py
+++ setup_web.py
@@ -4,14 +4,13 @@ import sys
 import zipfile
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path)
 
 if not os.path.exists('www'):
     sys.exit('Can\'t find www directory')
 
-os.system('cp -rf web/* www')
+command = 'cp -rf "{}/"* www'.format(os.path.normpath(os.path.join(dir_path, 'web')))
+#print(command)
+os.system(command)
 
-print(os.getcwd())
-
-with zipfile.ZipFile('assets/items.zip', 'r') as zf:
+with zipfile.ZipFile(os.path.join(dir_path, 'assets/items.zip'), 'r') as zf:
     zf.extractall('www/img')
