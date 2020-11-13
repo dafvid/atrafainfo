3edc5cd2bc979aa4be7885067c9371251c5d9cca
David S <david@dafnet.se>
2020-11-13 22:40:34 +0100

    move __main__.py to export.py

--- export.py
+++ export.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from atrafadata import export
+from atrafadata import export, eprint
 
 args = sys.argv
 
@@ -18,7 +18,13 @@ server_path = sys.argv[1]
 if not os.path.exists(server_path):
     sys.exit("Can't find minecraft data folder ({})".format(server_path))
 
-print(export(server_path, as_json=as_json))
+with open('www/index.html', 'w') as f:
+    f.writelines(export(server_path, test=True))
+    eprint("Created new www/index.html")
+
+with open('www/data.json', 'w') as f:
+    f.writelines(export(server_path, test=False, as_json=True))
+    eprint("Created new www/data.json")
 
 
 
