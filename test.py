46f1328197a1ca194cbd18d84d5bb25d6b7a37f3
David S <david@dafnet.se>
2020-11-13 22:40:19 +0100

    new test data

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 server_path = os.path.join(dir_path, 'test')
 
-
 with open('web/index.html', 'w') as f:
     f.writelines(export(server_path, test=True))
     eprint("Created new web/index.html")
