470be9a76af05ab9dd0e016bb6993dc7e2d7234a
David S <david@dafnet.se>
2020-11-13 21:27:25 +0100

    create data.json file #12

--- test.py
+++ test.py
@@ -10,3 +10,7 @@ server_path = os.path.join(dir_path, 'test')
 with open('web/index.html', 'w') as f:
     f.writelines(export(server_path, test=True))
     eprint("Created new web/index.html")
+
+with open('web/data.json', 'w') as f:
+    f.writelines(export(server_path, test=False, as_json=True))
+    eprint("Created new web/data.json")
